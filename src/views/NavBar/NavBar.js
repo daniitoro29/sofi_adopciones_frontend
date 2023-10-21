584853b0ed5fcf9f6a75b9c1fc8068221d96cf1f
santiago <santiagoparmenio2004@gmail.com>
2023-10-21 18:57:58 -0500

    agrege responsive de home

--- src/views/NavBar/NavBar.js
+++ src/views/NavBar/NavBar.js
@@ -1,67 +1,30 @@
-import { Redirect } from "react-router-dom";
 import { useState } from "react";
 import logo from '../../assets/img/logo5.png';
-
 import "../NavBar/NavBar.css";
 
 function NavBar() {
-    // URL logo
-    // https://logo-maker.freelogodesign.org/es/logo/edit/d273ed61a03749f5917f70b689935771
-  const [isRegister, setIsRegister] = useState(false);
-  const [showUser, setShowUser] = useState(false);
-  const [ isLogin, setIsLogin ] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handlerRegister = () => {
-    setIsRegister(true);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
-
-  const handlerUser = () => {
-    setShowUser(true);
-  };
-
-  const handlerLogin = () => {
-    setIsLogin(true);
-  }
-
-  if (isRegister) {
-    return <Redirect to="/register" />;
-  }
-  if (showUser) {
-    return <Redirect to="/users" />;
-  }
-  if (isLogin) {
-    return <Redirect to="/login" />;
-  }
 
   return (
     <div className="container-general_home">
       <nav>
-        <ul>
         <a href="/">
-        <img
-            src={logo}
-            alt="logo"
-          />
+          <img src={logo} alt="logo" />
         </a>
-
-          <ol>
-            <p>Inicio</p>
-          </ol>
-          <ol>
-            <p>Acerca de nosotros</p>
-          </ol>
-          <ol>
-            <p>Adopciones</p>
-          </ol>
-          <ol>
-            <p onClick={handlerRegister}>Registrate</p>
-          </ol>
-          <ol>
-            <p onClick={handlerUser}>Usuarios</p>
-          </ol>
-          <ol>
-            <p onClick={handlerLogin}>Ingresar</p>
-          </ol>
+        <button className="menu-button" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`menu-items ${isMenuOpen ? "open" : ""}`}>
+          <li>Inicio</li>
+          <li>Acerca de nosotros</li>
+          <li>Adopciones</li>
+          <li>Registrate</li>
+          <li>Usuarios</li>
+          <li>Ingresar</li>
         </ul>
       </nav>
     </div>
