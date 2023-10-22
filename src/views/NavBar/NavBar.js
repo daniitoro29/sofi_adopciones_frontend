7b8ba7b99a0b47d05e173ca9da71f292f5b41773
daniitoro29 <daniitoro29@gmail.com>
2023-10-21 20:49:12 -0500

    Update navbar functionality

--- src/views/NavBar/NavBar.js
+++ src/views/NavBar/NavBar.js
@@ -1,13 +1,39 @@
-import { useState } from "react";
-import logo from '../../assets/img/logo5.png';
 import "../NavBar/NavBar.css";
+import logo from '../../assets/img/logo5.png';
+import { Redirect } from "react-router-dom";
+import { useState } from "react";
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+  const [showUser, setShowUser] = useState(false);
+  const [ isLogin, setIsLogin ] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handlerRegister = () => {
+    setIsRegister(true);
+  };
+
+  const handlerUser = () => {
+    setShowUser(true);
+  };
+
+  const handlerLogin = () => {
+    setIsLogin(true);
+  }
+
+  if (isRegister) {
+    return <Redirect to="/register" />;
+  }
+  if (showUser) {
+    return <Redirect to="/users" />;
+  }
+  if (isLogin) {
+    return <Redirect to="/login" />;
+  }
 
   return (
     <div className="container-general_home">
@@ -22,9 +48,9 @@ function NavBar() {
           <li>Inicio</li>
           <li>Acerca de nosotros</li>
           <li>Adopciones</li>
-          <li>Registrate</li>
-          <li>Usuarios</li>
-          <li>Ingresar</li>
+          <li onClick={handlerRegister}>Registrate</li>
+          <li onClick={handlerUser}>Usuarios</li>
+          <li onClick={handlerLogin}>Ingresar</li>
         </ul>
       </nav>
     </div>
