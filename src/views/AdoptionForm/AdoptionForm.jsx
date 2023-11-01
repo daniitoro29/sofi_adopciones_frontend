8e009381cb8f2f812cff3c0c268ceba0be1159b1
daniitoro29 <daniitoro29@gmail.com>
2023-11-01 17:09:17 -0500

    Add styles

--- src/views/AdoptionForm/AdoptionForm.jsx
+++ src/views/AdoptionForm/AdoptionForm.jsx
@@ -8,29 +8,31 @@ import formato from "../../assets/pdf/formato.pdf";
 function Home() {
 
     return (
-        <div className="home">
+        <>
             <NavBar />
-            <h1>
-                ¿Estás interesado en cambiar vidas y darle un hogar a un ser necesitado?
-            </h1>
-            <h2>
-                ¡Entonces, estás en el lugar correcto! En SOFIadopciones, estamos buscando personas apasionadas y comprometidas que deseen adoptar y brindar amor a un compañero de cuatro patas.
-            </h2>
+            <div className="adoptions">
+                <h1>
+                    ¿Estás interesado en cambiar vidas y darle un hogar a un ser necesitado?
+                </h1>
+                <h2>
+                    ¡Entonces, estás en el lugar correcto! En SOFIadopciones, estamos buscando personas apasionadas y comprometidas que deseen adoptar y brindar amor a un compañero de cuatro patas.
+                </h2>
 
-            <p>
-                Para comenzar tu emocionante viaje hacia la adopción, descarga el formulario de solicitud haciendo clic en el enlace a continuación. Una vez que hayas completado el formulario con cariño y cuidado, envíalo a nuestro equipo a través del correo electrónico admin@sofiadopciones.com.
-            </p>
+                <p>
+                    Para comenzar tu emocionante viaje hacia la adopción, descarga el formulario de solicitud haciendo clic en el enlace a continuación. Una vez que hayas completado el formulario con cariño y cuidado, envíalo a nuestro equipo a través del correo electrónico admin@sofiadopciones.com.
+                </p>
 
-            <p>
-                Para descargar el formulario PDF, haga clic en el siguiente botón:
-            </p>
-            <button type="button" >
+                <p>
+                    Para descargar el formulario PDF, haga clic en el siguiente botón:
+                </p>
+                <button type="button" className="button-adoptions">
 
-                <a href={formato} target="_blank" rel="noopener noreferrer" download="adopciones.pdf">
-                    Descargar formulario PDF
-                </a>
-            </button>
+                    <a href={formato} target="_blank" rel="noopener noreferrer" download="adopciones.pdf">
+                        Descargar formulario PDF
+                    </a>
+                </button>
 
+            </div>
             <footer>
                 <div className="cont-redes-img">
                     <img src={facebook} alt="logo" />
@@ -38,7 +40,7 @@ function Home() {
                 </div>
                 <p>Copyright © SOF&</p>
             </footer>
-        </div>
+        </>
     );
 }
 
