941076c0363befe86f36b5083eeb78bf3929abc5
daniitoro29 <daniitoro29@gmail.com>
2023-11-01 15:55:17 -0500

    Add navbar

--- src/views/Gallery/Gallery.jsx
+++ src/views/Gallery/Gallery.jsx
@@ -4,6 +4,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import "../Gallery/Gallery.css";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
 
 
 const baseClassName = "gallery-container";
@@ -17,36 +18,40 @@ function Gallery() {
   }, [pets]);
 
   return (
-    <div className={`${baseClassName} d-block mx-auto`}>
-      <Carousel>
-        {pets.map((pet, index) => {
-          return pet.Mas_Estado_Adopcion === "Disponible" ? (
-            <Carousel.Item key={index} interval={15500} className={`${baseClassName}_carousel`}>
-              <img
-                className={`d-block w-auto ${baseClassName}_img`}
-                src={pet.Mas_Foto}
-                alt={pet.Mas_Nombre}
-              />
-              <Carousel.Caption>
-                <h3>{pet.Mas_Nombre}</h3>
-                <p>Especie: {pet.Mas_Especie}</p>
-                <p>Edad: {pet.Mas_Edad}</p>
-                <p>Raza: {pet.Mas_Raza}</p>
-                <p>Tamaño: {pet.Mas_Tamano}</p>
-                <p>Descripción: {pet.Mas_Descripcion}</p>
-                <Button
-                  variant="outlined"
-                  type="button"
-                  onClick={() => navigate("/adoptions")}
-                >
-                  Adopta a {pet.Mas_Nombre}
-                </Button>
-              </Carousel.Caption>
-            </Carousel.Item>
-          ) : null;
-        })}
-      </Carousel>
-    </div>
+    <>
+      <NavBar />
+      <div className={`${baseClassName} d-block mx-auto`}>
+
+        <Carousel>
+          {pets.map((pet, index) => {
+            return pet.Mas_Estado_Adopcion === "Disponible" ? (
+              <Carousel.Item key={index} interval={15500} className={`${baseClassName}_carousel`}>
+                <img
+                  className={`d-block w-auto ${baseClassName}_img`}
+                  src={pet.Mas_Foto}
+                  alt={pet.Mas_Nombre}
+                />
+                <Carousel.Caption>
+                  <h3>{pet.Mas_Nombre}</h3>
+                  <p>Especie: {pet.Mas_Especie}</p>
+                  <p>Edad: {pet.Mas_Edad}</p>
+                  <p>Raza: {pet.Mas_Raza}</p>
+                  <p>Tamaño: {pet.Mas_Tamano}</p>
+                  <p>Descripción: {pet.Mas_Descripcion}</p>
+                  <Button
+                    variant="outlined"
+                    type="button"
+                    onClick={() => navigate("/adoptions")}
+                  >
+                    Adopta a {pet.Mas_Nombre}
+                  </Button>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ) : null;
+          })}
+        </Carousel>
+      </div>
+    </>
   );
 }
 
