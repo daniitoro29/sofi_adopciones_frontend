5539f93fa7d478e38ac184d7ca9a6eefb0150482
daniitoro29 <daniitoro29@gmail.com>
2023-11-09 07:36:37 -0500

    Updated date in campaings

--- src/views/Campanas/Campanas.jsx
+++ src/views/Campanas/Campanas.jsx
@@ -19,18 +19,28 @@ const AllPets = () => {
     { field: "Fecha_Campana", headerName: "Cam_Fecha_Campana", width: 300 },
 ];
 
+
+const formatDate = (date) => {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const day = newDate.getDate();
+
+  return `${day}-${month}-${year}`;
+};
+
 const rows = campaigns.length > 0 ? campaigns.map((campaign) => ({
     id: campaign.Cam_Id,
     Lugar: campaign.Cam_Lugar,
     Descripcion: campaign.Cam_Descripcion,
-    Fecha_Campana: campaign.Cam_Fecha_Campana,
+    Fecha_Campana: formatDate(campaign.Cam_Fecha_Campana),
   })) : [];
 
   return (
     <>
       <NavBar />
       <div className="container-all_user">
-        <DataGrid rows={rows} columns={columns} checkboxSelection />
+        <DataGrid rows={rows} columns={columns} />
       </div>
     </>
   );
